b66f08c64980be5585e4c7a8f999ef447b6b2c4c
Adilton <adiltons28@gmail.com>
2023-06-10 15:38:53 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './css/main.css'
 
 function App() {
+  const [data, setData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => displayData(data))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const displayData = (data) =>{
+    setData(data)
+    setIsLoading(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+    {isLoading ? (
+        <div className="loader"></div>
+      ) : (
+        <>
+          <h6 className="advice-id">ADVICE# {data.slip.id}</h6>
+          <p className="advice-box">{data.slip.advice}</p>
+          <button onClick={fetchData} className="make-request">
+            <i class="fi fi-sr-dice"></i>
+          </button>
+          <span></span>
+        </>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
